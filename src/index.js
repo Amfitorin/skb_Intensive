aa6add280f7147786d2bcc79cee9e6db803d3e04
Amfitorin <Amfitorin@yandex.ru>
2016-11-12 19:18:45 +0300

    Task 2C Completed

--- src/index.js
+++ src/index.js
@@ -4,36 +4,13 @@ import cors from 'cors';
 const app = express();
 app.use(cors());
 app.get('/result2c', (req, res) => {
-  if (req.query.fullname === "")
-    res.send( "Invalid fullname" );
-  let result = req.query.fullname.replace(/\s+/g,' ').trim().split(' ');
-  for(var index in result)
-  {
-    if (result[index].search('[0-9_\/]')!= -1 )
-      res.send( "Invalid fullname" );
-    result[index] =  result[index][0].toUpperCase() + result[index].slice(1).toLowerCase();
-  }
-  
-  if ( result.length == 3 )
-  {
-    const name = result[0][0];
-    const sourName = result[2];
-    const fatherName = result[1][0];
-    res.send( `${sourName} ${name}. ${fatherName}.` );
-  }
-  else if (result.length == 2 )
-  {
-    const name = result[0][0];
-    const sourName = result[1];
-    res.send( `${sourName} ${name}.` );
-  }
-  else if (result.length == 1 )
-  {
-    const sourName = result[0];
-    res.send( `${sourName}` );
-  }
+  if (req.query.username === "")
+    res.send( "Invalid username" );
+  let name = req.query.username.match(/(^(?:\w+:)?(?:\/\/)?(?:[\w-_%]+\.[\w-_%]+)+\/)?\W?([\w\.]+)\/?/)[2];
+  if (name)
+    res.send( "@"+name );
   else
-    res.send( "Invalid fullname" );
+    res.send( "Invalid username" );
 });
 
 app.listen(3000, () => {
